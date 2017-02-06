796e4e088e065ccc70fb07158ba445ec4b65a349
vikas_chouhan <chouhanvikas95@gmail.com>
2017-02-06 18:01:43 +0000

    Ray casting done

--- Pixel.h
+++ Pixel.h
@@ -10,6 +10,12 @@ public:
 		r(0.0f), g(0.0f), b(0.0f) {
 	}
 
+	Pixel(Pixel &P)
+	{	r = P.r;
+		g = P.g;
+		b = P.b;
+	}
+
 	inline void colorPixel(float x ,float y, float z) {
 		r = x; g = y; b = z;
 	}
@@ -18,7 +24,12 @@ public:
 		r = 0; g = 0; b = 0;
 	}
 
-	
+	inline void Scale(float x)
+	{	r *= x;
+		g *= x;
+		b *= x;
+	}
+
 };
 
 
